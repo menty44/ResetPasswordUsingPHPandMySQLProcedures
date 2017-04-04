f81f94e8b297a746b5e9c2c919378c5a3a029b42
fred <menty44@gmail.com>
2017-04-04 16:07:26 +0300

    version 11

--- index.php
+++ index.php
@@ -39,13 +39,21 @@ $userid = $_GET['id'];
       <link rel="stylesheet" href="css/style.css">
 
     <script type="text/javascript">
-        $(function() {
-            $("#txtConfirmPassword").keyup(function() {
-                var password = $("#txtNewPassword").val();
-                $("#divCheckPasswordMatch").html(password == $(this).val() ? "Passwords match." : "Passwords do not match!");
-            });
-
-        });
+        function myFunction() {
+            var pass1 = document.getElementById("pass1").value;
+            var pass2 = document.getElementById("pass2").value;
+            var ok = true;
+            if (pass1 != pass2) {
+                //alert("Passwords Do not match");
+                document.getElementById("pass1").style.borderColor = "#E34234";
+                document.getElementById("pass2").style.borderColor = "#E34234";
+                ok = false;
+            }
+            else {
+                alert("Passwords Match!!!").style.borderColor = "#1e90ff";
+            }
+            return ok;
+        }
         </script>
 
 
@@ -61,7 +69,7 @@ $userid = $_GET['id'];
   <div class="login">
   	<h1 class="login-heading">
       <strong>Change Password.</strong> </h1>
-      <form method="post" name="frmChange" action="welcome.php/?id=<?php echo  $_GET["id"]; ?>"
+      <form method="post"  onsubmit="return myFunction()"   action="welcome.php/?id=<?php echo  $_GET["id"]; ?>  "  >
 <!--					<div class="message"><strong>
                             <font color="dodgerblue"><?php //if(isset($message)) { echo $message; } ?><!--</font>
                       </strong></div>
@@ -69,8 +77,8 @@ $userid = $_GET['id'];
 					<!--<?php echo $_POST["password"]; ?>
         <!--<input type="text" name="currentPassword" placeholder="Current Password" required="required" class="input-txt" id="currentPassword" />-->
 <!--      <p id="validate-status"></p>-->
-          <input type="password" name="password" placeholder="New Password" required="required" class="input-txt" id="password1"/>
-          <input type="password" name="confirmPassword" placeholder="Confirm Password" required="required" class="input-txt" id="password2" />
+          <input type="password" name="password" placeholder="New Password" required="required" class="input-txt" id="pass1"/>
+          <input type="password" name="confirmPassword" placeholder="Confirm Password" required="required" class="input-txt" id="pass2" />
 
       <div class="registrationFormAlert" id="divCheckPasswordMatch"></div>
 
