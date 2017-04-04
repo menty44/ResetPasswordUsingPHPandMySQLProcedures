9ec7c7db38094784f2e6e0d133b58674683e2024
fred <menty44@gmail.com>
2017-04-04 14:58:28 +0300

    version 7

--- index.php
+++ index.php
@@ -25,48 +25,7 @@ $userid = $_GET['id'];
 // 	} else $message = "Current Password is not correct";
 // }
 
-  $con = Database::getInstance()->getConnection();
-	$query = "CALL CHANGE_PASSWORD_EMAIL(:userid,:password,@status)";
-	$stmt = $con->prepare($query);
-	//$password = $_POST['password'];
-	$password = $_POST['password'];
-	$stmt->bindParam(":userid",$userid);
-	$pitiahapa = sha1($password);
-	//$stmt->bindParam(":password",$fields["password"]);
-	$stmt->bindParam(":password",$pitiahapa);
-	        if($stmt->execute()){
-						$result = $stmt->fetch(PDO::FETCH_OBJ);
-						            $stmt->closeCursor();
-												 //$status = $con->query("SELECT @status as status")->fetch(PDO::FETCH_OBJ);
-												 $status = $con->query("SELECT @status as status")->fetch(PDO::FETCH_OBJ);
-												          if($status->status == "0000"){
-																		//$response = array("status_code"=>"0000","message"=>"Success");
-																		//$response = '{"status_code"=>"0000","message"=>"Success"}';
-																	//	$this->response(json_encode($response),200,$this->callback);
-																	$response = '{"status_code":"0000","message":"Password Change was Successful"}';
-																	$fredshome = json_decode($response);
-																	// access message $book object
 
-																	$umoja = $fredshome->message;
-																	echo '<h1 class="login-heading"><center><font color="yellow">'.$umoja.'</font></center></h1>';
-
-
-																	    }else{
-																				//$response = array("status_code"=>$status->status,"message"=>"Please Try changing your password Again");
-																				$response =  '{"status_code":"0021","message":"Please Try changing your password Again"}';
-																				$fredshomemum = json_decode($response);
-																				// access title of $book object
-																				$buruburu = $fredshomemum->message;
-																				echo '<h1 class="login-heading"><center><font color="red">'.$buruburu.'</font></center></h1>';
-																			//	$this->response(json_encode($response),200,$this->callback);
-																			//echo json_encode($response) ;
-																			  }
-																			  }else{
-																					$error = $stmt->errorInfo();
-																					$response = array("status_code"=>$stmt->errorCode(),"message"=>"Error inserting to the database.".$error[2]);
-																					//$this->response(json_encode($response),200,$this::callback);
-																					echo json_encode($response) ;
-																				       }
 ?>
 
 <html >
@@ -126,12 +85,16 @@ $userid = $_GET['id'];
   <div class="login">
   	<h1 class="login-heading">
       <strong>Change Password.</strong> </h1>
-      <form method="post" name="frmChange" action="" onSubmit="return validatePassword()">
-					<div class="message"><strong><font color="yellow"><?php if(isset($message)) { echo $message; } ?></font></strong></div>
+      <form method="post" name="frmChange" action="welcome.php/?id=<?php echo  $_GET["id"]; ?>" onSubmit="return validatePassword()">
+					<div class="message"><strong>
+                            <font color="dodgerblue"><?php if(isset($message)) { echo $message; } ?></font>
+                        </strong></div>
 					<!--<?php echo $_POST["password"]; ?>-->
         <!--<input type="text" name="currentPassword" placeholder="Current Password" required="required" class="input-txt" id="currentPassword" />-->
           <input type="password" name="password" placeholder="New Password" required="required" class="input-txt" id="password"/>
 					<input type="password" name="confirmPassword" placeholder="Confirm Password" required="required" class="input-txt" id="confirmPassword"/>
+
+
           <div class="login-footer">
              <a href="#" class="lnk">
 
@@ -147,3 +110,4 @@ $userid = $_GET['id'];
 
 </body>
 </html>
+
---
+++ welcome.php
@@ -0,0 +1,64 @@
+<?php
+
+require_once "lib/Database.php";
+require_once "lib/nusoap.php";
+//require_once "lib/Rest.inc.php";
+require_once "utilities.php";
+
+/**
+ * Created by PhpStorm.
+ * User: fred
+ * Date: 4/4/17
+ * Time: 2:17 PM
+ */
+
+$userid = $_GET['id'];
+
+//echo $userid;
+
+$con = Database::getInstance()->getConnection();
+$query = "CALL CHANGE_PASSWORD_EMAIL(:userid,:password,@status)";
+$stmt = $con->prepare($query);
+//$password = $_POST['password'];
+$password = $_POST['password'];
+$stmt->bindParam(":userid",$userid);
+$pitiahapa = sha1($password);
+//$stmt->bindParam(":password",$fields["password"]);
+$stmt->bindParam(":password",$pitiahapa);
+if($stmt->execute()){
+    $result = $stmt->fetch(PDO::FETCH_OBJ);
+    $stmt->closeCursor();
+    //$status = $con->query("SELECT @status as status")->fetch(PDO::FETCH_OBJ);
+    $status = $con->query("SELECT @status as status")->fetch(PDO::FETCH_OBJ);
+    if($status->status == "0000"){
+        //$response = array("status_code"=>"0000","message"=>"Success");
+        //$response = '{"status_code"=>"0000","message"=>"Success"}';
+        //	$this->response(json_encode($response),200,$this->callback);
+        $response = '{"status_code":"0000","message":"Password Change was Successful"}';
+        $fredshome = json_decode($response);
+        // access message $book object
+
+        $umoja = $fredshome->message;
+        echo '<h1 class="login-heading"><center><font color="dodgerblue">'.$umoja.'</font></center></h1>';
+
+
+    }else{
+        //$response = array("status_code"=>$status->status,"message"=>"Please Try changing your password Again");
+        $response =  '{"status_code":"0021","message":"Please Try changing your password Again"}';
+        $fredshomemum = json_decode($response);
+        // access title of $book object
+        $buruburu = $fredshomemum->message;
+        echo '<h1 class="login-heading"><center><font color="red">'.$buruburu.'</font></center></h1>';
+        //	$this->response(json_encode($response),200,$this->callback);
+        //echo json_encode($response) ;
+    }
+}else{
+    $error = $stmt->errorInfo();
+    $response = array("status_code"=>$stmt->errorCode(),"message"=>"Error inserting to the database.".$error[2]);
+    //$this->response(json_encode($response),200,$this::callback);
+    echo json_encode($response) ;
+}
+
+
+?>
+
